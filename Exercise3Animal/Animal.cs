6269071260d3433700420cc1cc0f94fd99c81955
Radha Manickam <radhaarul@gmail.com>
2021-12-09 14:50:54 +0100

    Add project files.

---
+++ Exercise3Animal/Animal.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3Animal
+{
+    //3.2.14F
+    //[AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)] with help if this code we can add
+    // new Attribute to the Parent Class Animal
+    public abstract class Animal //Abstract Class , :Attribute (for to create new attribute)
+    {
+        string Name;
+        double Weight;
+        int Age;
+        public string name { get; set; }
+        public double weight { get; set; }
+        public int age { get; set; }
+
+        public abstract void DoSound(); //Abstract Method
+        public Animal()
+        {
+        }
+        public virtual string Stats()
+        {
+            return ($"{name} {weight} {age} ");
+        }
+    }
+    public class Horse : Animal // Class Animal inherit in the child class Horse
+    {
+        private string Gallop = "Gallop";
+        public override void DoSound() // Abstract Method declared here with override keyword
+        {
+            Console.WriteLine("Horse Says : neigh");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {Gallop}");
+        }
+        public Horse(string name, double weight, int age, string gallop)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            Gallop = gallop;
+        }
+    }
+    public class Dog : Animal
+    {
+        private string CoolPet = "CoolPet";
+        public override void DoSound()
+        {
+            Console.WriteLine("Dog Says : bow-wow");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {CoolPet}");
+        }
+        public Dog(string name, double weight, int age, string Coolpet)
+        {
+            this.name = name;
+            this.weight = weight;
+            CoolPet = Coolpet;
+        }
+        public Dog() { }
+        public string color() //15. Create a new method with any name in the Dog class that returns only an optional string.
+        {
+            return "Dogs are black, brown,grey and white in color";
+        }
+        //16. Do you access that method from the Animals list?
+        //17. F: Why not?
+        // i can access this method from the Animal list by using casting
+    }
+    public class Hedgehog :Animal
+    {
+        private string NrOfSpikes;
+        public override void DoSound()
+        {
+            Console.WriteLine("Hedgehog Says : growl");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {NrOfSpikes}");
+        }
+        public Hedgehog(string name, double weight, int age, string nrofspikes)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            NrOfSpikes = nrofspikes;
+        }
+    }
+    class Worm : Animal
+    {
+        private string IsPoisonous;
+        public override void DoSound()
+        {
+            Console.WriteLine("Worm Says : SSSSSS");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {IsPoisonous}");
+        }
+        public Worm(string name, double weight, int age,string ispoisonous)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            IsPoisonous = ispoisonous;
+        }
+    }
+    //3.2.13F
+    //[AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)] with help if this code we can add
+    // new Attribute to the Parent Class Bird
+    class Bird : Animal //:Attribute
+    {
+        private string WingSpan;
+        public override void DoSound()
+        {
+            Console.WriteLine("Bird Says : Chirp");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {WingSpan}");
+        }
+        public Bird(string name, double weight, int age)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            
+        }
+    }
+    class Wolf : Animal
+    {
+        private string KeenSenses;
+        public override void DoSound()
+        {
+            Console.WriteLine("Wolf Says : Owooooo");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {KeenSenses}");
+        }
+        public Wolf(string name, double weight, int age)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+           
+        }
+    }
+    class Pelican : Bird // Parent Class Bird inherit in the child class Pelican
+    {
+        private string LargeBodyShortLeg;
+        public override void DoSound()
+        {
+
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {LargeBodyShortLeg}");
+        }
+        public Pelican(string name, double weight, int age, string largebodyshortleg) :base( name, weight, age)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            LargeBodyShortLeg = largebodyshortleg;
+        }
+    }
+    class Flamingo : Bird
+    {
+        private string VeryLargeNeck;
+        public override void DoSound()
+        {
+
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {VeryLargeNeck}");
+        }
+        public Flamingo(string name, double weight, int age,string verylargeneck) : base(name, weight, age)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            VeryLargeNeck = verylargeneck;
+        }
+    }
+    class Swan : Bird
+    {
+        private string WorldBeauty;
+        public override void DoSound()
+        {
+
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} {WorldBeauty}");
+        }
+        public Swan(string name, double weight, int age,string worldbeauty) : base(name, weight, age)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            WorldBeauty = worldbeauty;
+        }
+    }
+    class Wolfman : Wolf, Person //Class that inherits from Wolf and implements the IPerson interface.
+    {
+        public void Talk()
+        {
+            Console.WriteLine("Wolfman says : Roar");
+        }
+        public override string Stats()
+        {
+            return ($"{name} {weight} {age} ");
+        }
+        public Wolfman(string name, double weight, int age) : base(name, weight, age)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age; ;
+        }
+    }
+}
---
+++ Exercise3Animal/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Exercise3Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            Animal hr, wm;
+            hr = new Horse("HorseA",30,12,"very strong animal");
+            wm = new Wolfman("wolfmanA",120,8);
+            animals.Add(hr);
+            animals.Add(wm);
+
+            foreach (var a in animals)
+            {
+                if (a.GetType().Name == "Wolfman")
+                {
+                    Console.WriteLine(a.Stats());
+                    Person per = (Person)a;
+                    per.Talk();
+                }
+                else
+                {
+                    Console.WriteLine(a.Stats());
+                    a.DoSound();
+                }
+            }
+            Console.WriteLine();
+
+            List<Dog> dogs = new List<Dog>(); // create a list for dogs
+                                              // dogs.Add(hr); // 9. F: Try adding a horse to the list of dogs. Why does it not work?
+                                              // because they both have same parent Animal but there is no relationship between dog and horse.
+                                              //10. F: What type must the list be in order for all classes to be stored together?
+                                              // Type should be Animal in order to store all classes.
+
+            // create all animals
+            List<Animal> Allanimals = new List<Animal>();
+            Animal Hrs, Dg,Dg1, Hd, Wo, Pe, Fl, Sw;
+            Hrs = new Horse("Horse1",30,18,"Gallop"); 
+            Dg = new Dog("Dog1",25,9,"Coolpet");
+            Dg1 = new Dog("Dog2", 20, 8, "Coolpet");
+            Hd = new Hedgehog("Hedgehog1",.1,1,"Has lot of spikes");
+            Wo = new Worm("Worm1",.1,1,"This is poisonous");
+            Pe = new Pelican("Pelican1",5,2,"it has very large body and short legs");
+            Fl = new Flamingo("Flamingo1",7,3,"it has very large neck"); 
+            Sw = new Swan("Swan1",3,2,"its worlds beauty");
+            Allanimals.Add(Hrs); Allanimals.Add(Dg); Allanimals.Add(Hd); Allanimals.Add(Wo); 
+            Allanimals.Add(Pe); Allanimals.Add(Fl); Allanimals.Add(Sw);Allanimals.Add(Dg1);
+
+            //11. Print all Animals Stats () through a foreach loop.
+            foreach (var A in Allanimals)
+                Console.WriteLine(A.Stats());
+
+            //13. F: Explain what is happening.
+            // because of the keyword override it calles only corresponding devrived class Stats() method
+
+            //14. Print the Stats () method for all dogs only through a foreach on Animals.
+            Console.WriteLine();
+            
+            foreach ( var A in Allanimals)
+            {
+                if (A.GetType().Name == "Dog")
+                {
+                    Console.WriteLine(A.Stats());
+                    Dog dd = (Dog)A;
+                    string col = dd.color();
+                    Console.WriteLine(col);
+                    
+                }
+                    
+            }
+        }
+    }
+}
